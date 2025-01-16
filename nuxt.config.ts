427d217c10cc8a219a3ca808bae8f808194e950e
FLY123 <akfakf0509@naver.com>
2025-01-16 23:50:10 +0900

    chore: install vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-07-27",
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@pinia/nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+  ],
   eslint: {},
 });
