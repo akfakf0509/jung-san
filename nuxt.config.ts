1852585ae31d3ec39356920201985cdfa6ceb6c0
FLY123 <akfakf0509@naver.com>
2025-01-17 00:15:26 +0900

    chore: install vueuse/core

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-07-27",
+  imports: {
+    dirs: ["types"],
+  },
   modules: [
     "@nuxt/eslint",
     "@nuxt/ui",
