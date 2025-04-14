5a9955f7b2da82e8b61771ae6e52cfb80d77e115
FLY123 <akfakf0509@naver.com>
2025-04-14 23:36:49 +0900

    feat: 버튼 컴포넌트 생성

---
+++ components/JSButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+defineProps<{
+  label?: string;
+}>();
+</script>
+
+<template>
+  <button
+    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+  >
+    <slot>{{ label }}</slot>
+  </button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Jung san</div>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+  >
+    <h1 class="text-4xl font-bold text-gray-800">정산하러 가기</h1>
+    <JSButton label="시작하기" />
+  </div>
 </template>
