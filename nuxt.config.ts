4cf35089fbdfa7ccc5a7424c66d5c45efc7dfbcd
FLY123 <akfakf0509@naver.com>
2025-04-14 23:24:24 +0900

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-07-27",
@@ -6,4 +8,8 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxt/eslint", "@pinia/nuxt", "@vueuse/nuxt", "reka-ui/nuxt"],
   eslint: {},
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
 });
